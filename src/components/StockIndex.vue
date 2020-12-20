7d7f19fc29a749d897bf4b90e657bafaae56237b
Aijun Li <liaijun.cn@gmail.com>
2020-12-20 21:24:28 +0800

    Build component StockIndex

---
+++ src/components/StockIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-row>
+    <el-col v-for="index of indexList" :key="index" :xs="8" :sm="4">
+      <stock-index-item :name="index"></stock-index-item>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import StockIndexItem from './StockIndexItem.vue'
+
+export default defineComponent({
+  components: {
+    StockIndexItem
+  },
+  setup() {
+    const indexList = [
+      '上证指数',
+      '深证成指',
+      '创业板指',
+      '沪深300',
+      '上证50',
+      '中证500'
+    ]
+
+    return { indexList }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.el-row {
+  padding-right: 1rem;
+}
+.el-col {
+  padding: 0.25rem 0;
+}
+</style>
--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,9 +1,9 @@
-import { ElButton } from 'element-plus'
+import { ElButton, ElRow, ElCol } from 'element-plus'
 import lang from 'element-plus/lib/locale/lang/zh-cn'
 import locale from 'element-plus/lib/locale'
 import { App } from 'vue'
 
-const components = [ElButton]
+const components = [ElButton, ElRow, ElCol]
 
 export default (app: App) => {
   locale.use(lang)
