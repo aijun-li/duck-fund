f73310e934ea0247738c564baf91aed703f2905f
Aijun Li <liaijun.cn@gmail.com>
2020-12-23 13:53:32 +0800

    Construct Home view

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,9 @@ import {
   createWebHistory,
   RouteRecordRaw
 } from 'vue-router'
+import Home from '@/views/Home.vue'
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [{ path: '/', component: Home }]
 
 const router = createRouter({
   history: process.env.IS_ELECTRON
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <stock-index
+    style="padding-top: 0.3rem; padding-bottom: 0.3rem"
+  ></stock-index>
+  <div class="divider"></div>
+  <fund-list></fund-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from '@/store'
+import FundList from '@/components/FundList.vue'
+import StockIndex from '@/components/StockIndex.vue'
+
+export default defineComponent({
+  components: {
+    StockIndex,
+    FundList
+  },
+  setup() {
+    const store = useStore()
+    store.commit('initializeFunds', {
+      funds: localStorage.getItem('storedFunds')!
+    })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.divider {
+  height: 5px;
+  background-color: #e6e6e6;
+}
+</style>
