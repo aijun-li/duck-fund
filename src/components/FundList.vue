ee8600479ca923222172efe6b43a78bdf89f89a0
Aijun Li <liaijun.cn@gmail.com>
2020-12-23 13:43:18 +0800

    Display the date of value in FundList

--- src/components/FundList.vue
+++ src/components/FundList.vue
@@ -17,12 +17,21 @@
     </el-table-column>
     <el-table-column
       prop="dwjz"
-      label="净值"
       header-align="right"
       sortable
       :sort-method="(a, b) => a.jzzl - b.jzzl"
       min-width="1"
     >
+      <template #header>
+        <!-- push the header to the right -->
+        <div style="flex: 1"></div>
+        <div>
+          <div>净值</div>
+          <div class="value-date">
+            {{ valueDate.net.slice(5, 10) }}
+          </div>
+        </div>
+      </template>
       <template #default="scope">
         <div class="value-cell">
           <div>
@@ -39,16 +48,31 @@
           >
             {{ (scope.row.jzzl > 0 ? '+' : '') + scope.row.jzzl }}%
           </div>
+          <div
+            v-if="scope.row.jzrq && scope.row.jzrq !== valueDate.net"
+            class="value-date"
+          >
+            {{ scope.row.jzrq.slice(5, 10) }}
+          </div>
         </div>
       </template>
     </el-table-column>
     <el-table-column
-      label="估值"
       sortable
       :sort-method="(a, b) => a.gszzl - b.gszzl"
       header-align="right"
       min-width="1"
     >
+      <template #header>
+        <!-- push the header to the right -->
+        <div style="flex: 1"></div>
+        <div>
+          <div>估值</div>
+          <div class="value-date">
+            {{ valueDate.estimate.slice(5, 10) }}
+          </div>
+        </div>
+      </template>
       <template #default="scope">
         <div class="value-cell">
           <div>{{ scope.row.gsz }}</div>
@@ -71,12 +95,12 @@
 
 <script lang="ts">
 import { AxiosStatic } from 'axios'
-import { computed, defineComponent, inject, onMounted } from 'vue'
+import { computed, defineComponent, inject, onMounted, reactive } from 'vue'
 import { State, useStore } from '@/store'
 import { Store } from 'vuex'
 import StockPrice from '@/interfaces/StockPrice'
 
-function fetchData(axios: AxiosStatic, store: Store<State>) {
+function fetchData(axios: AxiosStatic, store: Store<State>, valueDate: any) {
   const funds = computed(() => store.state.funds)
 
   function fetchPrice() {
@@ -96,6 +120,24 @@ function fetchData(axios: AxiosStatic, store: Store<State>) {
         `https://www.dayfund.cn/ajs/ajaxdata.shtml?showtype=getfundvalue&fundcode=${fund.fundcode}`
       )
       price.jzzl = parseFloat(response2.split('|')[4].slice(0, -1)).toFixed(2)
+      price.jzrq = response2.split('|')[0]
+      price.dwjz = response2.split('|')[1]
+
+      // update the displayed date of value
+      if (
+        !valueDate.net ||
+        new Date(valueDate.net).getTime() < new Date(price.jzrq!).getTime()
+      ) {
+        valueDate.net = price.jzrq
+      }
+      if (
+        !valueDate.estimate ||
+        new Date(valueDate.estimate).getTime() <
+          new Date(price.gztime!.split(' ')[0]).getTime()
+      ) {
+        valueDate.estimate = price.gztime
+      }
+
       store.commit('updateFund', price)
     })
   }
@@ -108,14 +150,15 @@ export default defineComponent({
     const axios = inject<AxiosStatic>('axios')!
     const store = useStore()
 
-    const { funds, fetchPrice } = fetchData(axios, store)
+    const valueDate = reactive({ net: '', estimate: '' })
+    const { funds, fetchPrice } = fetchData(axios, store, valueDate)
 
     fetchPrice()
     onMounted(() => {
       setInterval(fetchPrice, 10000)
     })
 
-    return { funds }
+    return { funds, valueDate }
   }
 })
 </script>
@@ -127,7 +170,12 @@ export default defineComponent({
 
 .value-cell {
   text-align: right;
-  padding-right: 0.8rem;
+  padding-right: 1rem;
+}
+
+.value-date {
+  font-size: 0.8rem;
+  color: grey;
 }
 
 .change-percent {
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import installFontAwesome from './plugins/font-awesome'
 import axios from 'axios'
 
 import './styles/global.css'
+import './styles/element.scss'
 
 const app = createApp(App)
 
